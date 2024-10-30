456cee27036e1452037a04c4200ee8dc6427917d
= <abdoahmedsayed704@gmail.com>
2024-10-30 16:49:07 +0300

    Done..

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -3,6 +3,15 @@ import math
 from typing import List, Tuple
 
 
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    tuple of size two containing a start and end index.
+    """
+    start_index = (page - 1) * page_size
+    end_index = page * page_size
+    return (start_index, end_index)
+
+
 class Server:
     """Server class to paginate a database of popular baby names.
     """
@@ -22,20 +31,15 @@ class Server:
 
         return self.__dataset
 
-    def index_range(slef, page: int, page_size: int) -> Tuple[int, int]:
-        """
-        tuple of size two containing a start and end index.
-        """
-        start_index = (page - 1) * page_size
-        end_index = page * page_size
-        return (start_index, end_index)
-
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """
-        return a spec page"""
-        assert page > 0
-        assert type(page_size) is int
-        assert type(page) is int
-        self.dataset()
-        (start_index, end_index) = self.index_range(page, page_size)
-        return [self.__dataset[start_index: end_index]]
+        return spec items
+        """
+        assert type(page_size) is int and type(page) is int
+        assert page > 0 and page_size > 0
+
+        data = self.dataset()
+        (start_index, end_index) = index_range(page, page_size)
+        if start_index > len(data):
+            return []
+        return data[start_index: end_index]
